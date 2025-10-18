695095cb9baca3aa5f5c15aa90f9e80638104b50
Hoan7732 <hoan77123@gmail.com>
2025-10-19 00:51:22 +0700

    first commit

---
+++ app/src/main/java/com/example/btl_nhom1/app/presentation/adapter/SliderAdapter.java
@@ -0,0 +1,50 @@
+package com.example.btl_nhom1.app.presentation.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.btl_nhom1.R;
+
+import java.util.List;
+
+public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderViewHolder> {
+
+    private final List<Integer> imageList;
+
+    public SliderAdapter(List<Integer> imageList) {
+        this.imageList = imageList;
+    }
+
+    @NonNull
+    @Override
+    public SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_home_slider, parent, false);
+        return new SliderViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SliderViewHolder holder, int position) {
+        holder.imageView.setImageResource(imageList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageList.size();
+    }
+
+    static class SliderViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        SliderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageSlider);
+        }
+    }
+}
+
